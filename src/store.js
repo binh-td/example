22a052915d9e414aac5f991e4117e981195a3ad8
binh-td <binhtd.com@gmail.com>
2022-11-11 21:09:02 +0700

    update

--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './redux/Login/authSlice';
-
-export const store = configureStore({
-  reducer: { auth: authReducer },
-})
